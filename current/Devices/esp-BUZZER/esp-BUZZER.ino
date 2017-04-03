a5f8f77f63dc4f683b6a632d46f1f1366e5ba53e
Richard42Graham <up628887@myport.ac.uk>
2017-04-03 22:34:17 +0100

    buzzer

--- current/Devices/esp-BUZZER/esp-BUZZER.ino
+++ current/Devices/esp-BUZZER/esp-BUZZER.ino
@@ -11,9 +11,11 @@ AutoHome autohome;
 #define dd 16
 #define ee 4
 
-char m0de = "a";  // mode selelct
+int m0de = 0;  // mode selelct
 int buz = 0;     // 0 = off// 1 = buzz// 2 = beep//
 int watier = 0; // time its buzzing for
+
+unsigned long previouseMillis = 0;
 
 
 void setup() {
@@ -105,32 +107,32 @@ void M0DE()
 {
   switch (m0de)
     {
-      case : "a"
+      case 0:
         {
             digitalWrite(aa, HIGH);  digitalWrite(bb, HIGH);  digitalWrite(cc, HIGH);  digitalWrite(dd, HIGH);digitalWrite(ee, HIGH);
         }   break;
 
-      case : "b"
+      case  1 :
         {
             digitalWrite(aa, LOW);  digitalWrite(bb, HIGH);  digitalWrite(cc, HIGH);  digitalWrite(dd, HIGH);  digitalWrite(ee, HIGH);
         }   break;
 
-      case : "c"
+      case  2 :
         {
             digitalWrite(aa, HIGH);  digitalWrite(bb, LOW);  digitalWrite(cc, HIGH);  digitalWrite(dd, HIGH);  digitalWrite(ee, HIGH);
         }   break;
 
-      case : "d"
+      case  3 :
         {
             digitalWrite(aa, HIGH);  digitalWrite(bb, HIGH);  digitalWrite(cc, LOW);  digitalWrite(dd, HIGH);  digitalWrite(ee, HIGH);
         }   break;
 
-      case : "e"
+      case  4 :
         {
             digitalWrite(aa, HIGH);  digitalWrite(bb, HIGH);  digitalWrite(cc, HIGH);  digitalWrite(dd, LOW);  digitalWrite(ee, HIGH);
         }   break;
 
-      case : "f"
+      case  5 : 
         {
             digitalWrite(aa, HIGH);  digitalWrite(bb, HIGH);  digitalWrite(cc, HIGH);  digitalWrite(dd, HIGH);  digitalWrite(ee, LOW);
         }   break;
@@ -140,25 +142,28 @@ void M0DE()
 
 void BUZZ() 
 {
-  M0DE();
+ // M0DE();
    switch(buz)
    {
-      case : '0'
+      case 0:
         {
           digitalWrite(buzz,LOW);
           digitalWrite(beep,LOW);
+          Serial.println("offing");
         } break;
 
-       case : '1'
+       case 1 :
         {
           digitalWrite(buzz,HIGH);
           digitalWrite(beep,LOW);
+          Serial.println("buzzing");
         } break;
 
-       case : '2'
+       case 2 :
         {
           digitalWrite(buzz,LOW);
           digitalWrite(beep,HIGH);
+          Serial.println("beeping");
         } break; 
    }
 }
@@ -168,40 +173,58 @@ void BUZZ()
 /* Gets called when get message from any topic*/
 void mqtt_callback(char* topic, byte* payload, unsigned int length) 
 {
+  if (autohome.mqtt_callback(topic,payload, length) == 0){
   String packet = "";
+
 
   for (int i = 0; i < length; i++) 
     {
       packet = packet + (char)payload[i];
     }
-              if (getValue(packet, ':', 0).equals("BUZZ")) 
+
+              if (autohome.getValue(packet, ':', 0).equals("BUZZ")) 
               { // example // BUZZ:a:200    -- log wired buzz
-                  m0de = (getValue(packet, ':', 1));    // the mode the buzzer is in.
-                  watier = (getValue(packet, ':', 2));  // the how long the beep lasts. 
+                 buz = 1;
+                 m0de = (autohome.getValue(packet, ':', 1)).toInt();    // the mode the buzzer is in.
+                  watier = (autohome.getValue(packet, ':', 2)).toInt();  // the how long the beep lasts. 
             
-                 String packet = "RGB:IS:" + String(currR) + "," + String(currG) + "," + String(currB);
-                 autohome.sendPacket( packet.c_str() );
+                 // String packet = "RGB:IS:" + String(currR) + "," + String(currG) + "," + String(currB);
+               //  autohome.sendPacket( packet.c_str() );
+
+String packetOUT = "BUZZER:IS:" + String(m0de) + "," + String(buz) + "," + String(watier);
+autohome.sendPacket( packetOUT.c_str() );
+Serial.println(packetOUT);
               }
 
-
-          if (getValue(packet, ':', 0).equals("BEEP"))
+          if (autohome.getValue(packet, ':', 0).equals("BEEP"))
               { 
-                  m0de = "a";  // mode selelct
-                  buzz = 2; // 0 = off// 1 = buzz// 2 = beep//
-                  watier = (getValue(packet, ':', 2));  // the how long the beep lasts.
-              }
-              
-String packet = "BUZZER:IS:" + String(m0de) + "," + String(buzz) + "," + String(watier);
-autohome.sendPacket( packet.c_str() );
+                  buz = 2; // 0 = off// 1 = buzz// 2 = beep//
+                  m0de = (autohome.getValue(packet, ':', 1)).toInt();    // the mode the buzzer is in.
 
-}
+                   watier = (autohome.getValue(packet, ':', 2)).toInt();  // the how long the beep lasts.
+String packetOUT = "BUZZER:IS:" + String(m0de) + "," + String(buz) + "," + String(watier);
+autohome.sendPacket( packetOUT.c_str() );
+Serial.println(packetOUT);
+              }
+
+if (autohome.getValue(packet, ':', 0).equals("NO"))
+              { 
+                  buz = 0; // 0 = off// 1 = buzz// 2 = beep//
+                  m0de = 0;
+
+                   watier = 1; 
+              } 
+
+}}
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
 unsigned long currentMillis = millis();
 
 autohome.loop();
-
+delay(1);
 
 // recive MQTT comand 
 // example // BUZZ:a:200    -- log wired buzz
@@ -210,23 +233,21 @@ autohome.loop();
  //   a - z   = buzz combinations 
  //   0-999   = time in seconds? 
 
- unsigned long currentMillis = millis();
-
-  process_fade(currentMillis);
-
-  if (currentMillis - previousMillisKeypad >= 10)
+  if (currentMillis - previouseMillis >= 10)
               {
             
-             previousMillisKeypad = currentMillis;
-             void MODE();
-             void BUZZ();
-
-             watier = (watier - 1);
-
+             previouseMillis = currentMillis;
+              M0DE();
+              BUZZ();
+Serial.println("iping");
               if(watier == 0)
                   {
-                    char m0de = "a";  // mode selelct
-                    int buz = 0;     // 0 = off// 1 = buzz// 2 = beep//
+                     m0de = 0;  // mode selelct
+                     buz = 0;     // 0 = off// 1 = buzz// 2 = beep//
+                     Serial.println("stopping");
                   }
+                      else
+                          { watier = (watier - 1);
+                             Serial.println("counting"); }
               }
 }
