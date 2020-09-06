40b0ad62825e246dcc7deb665e99e8e8ecc4a0f1
Jens Larsen <mr.javawolf@gmail.com>
2020-09-06 19:45:45 +0200

    Added: Easing functions

--- current/Devices/Bathroom-lights/Bathroom-lights/Bathroom-lights.ino
+++ current/Devices/Bathroom-lights/Bathroom-lights/Bathroom-lights.ino
@@ -1,5 +1,6 @@
 /* This example is a simple setup example to show you how to setup the AutoHome library. */
 
+// Bathroom lights are running on a ESP 8622
 #define MQTT_VERSION MQTT_VERSION_3_1
 
 #include <AutoHome.h>
@@ -30,7 +31,7 @@ enum LightState
 struct TurnOnAnimation
 {
   // How long the turn on animation should be in ms
-  float AnimationTimeMs = 200;
+  float AnimationTimeMs = 2000;
 
   // How wide the highlight should be in number of Leds
   int HighlightWidth = 5;
@@ -41,10 +42,19 @@ struct TurnOnAnimation
   int HighlightAM = 255;
 };
 
+enum TurnOffAnimationType
+{
+  Across = 1,
+  MiddleAndOut = 2,
+};
+
 struct TurnOffAnimation
 {
   // How long the turn off animation should be in ms
   float AnimationTimeMs = 200;
+
+  // Which animation type it should play
+  TurnOffAnimationType AnimationType = Across;
 };
 
 TurnOnAnimation turnOnAnimation;
@@ -70,6 +80,10 @@ void set_state(LightState new_state)
 {
   state = new_state;
   stateStartTime = millis();
+  if (TurningOff == new_state)
+  {
+    turnOffAnimation.AnimationType = (TurnOffAnimationType)random(1, 3);
+  }
 }
 
 void mqtt_send_stats()
@@ -159,7 +173,6 @@ void mqtt_callback(char *topic, byte *payload, unsigned int length)
 
 void setup()
 {
-
   Serial.begin(115200);
 
   // On/Off button
@@ -220,7 +233,14 @@ void loop()
   }
   else if (state == TurningOff)
   {
-    TurnOffAnimation_Loop();
+    if (turnOffAnimation.AnimationType == Across)
+    {
+      TurnOffAnimation1_Loop();
+    }
+    else
+    {
+      TurnOffAnimation2_Loop();
+    }
   }
 }
 
@@ -232,12 +252,17 @@ void TurnOnAnimation_Loop()
   {
     // Do the turn on animation
     float ratio = timeSinceStart / turnOnAnimation.AnimationTimeMs;
-    int pixelsTurnedOn = NUMBER_OF_PIXELS * ratio;
+    int pixelsTurnedOn = NUMBER_OF_PIXELS * easeOutCubic(ratio);
 
     //Sets the first leds to the wanted AM, CW, WW color
     for (int i = 0; i < pixelsTurnedOn && i < NUMBER_OF_PIXELS; i++)
     {
       pixels.setPixelColor(i, pixels.Color(AM, CW, WW));
+    }
+
+    for (int i = pixelsTurnedOn; i < NUMBER_OF_PIXELS; i++)
+    {
+      pixels.setPixelColor(i, pixels.Color(0, 0, 0));
     }
 
     // Sets the highlight colors infront of the normal wanted colors
@@ -265,7 +290,7 @@ void TurnOnAnimation_Loop()
   }
 }
 
-void TurnOffAnimation_Loop()
+void TurnOffAnimation1_Loop()
 {
 
   unsigned long currentTime = millis();
@@ -273,11 +298,42 @@ void TurnOffAnimation_Loop()
   if (timeSinceStart < turnOffAnimation.AnimationTimeMs)
   {
     // Do the turn on animation
-    float ratio = timeSinceStart / turnOnAnimation.AnimationTimeMs;
+    float ratio = timeSinceStart / turnOffAnimation.AnimationTimeMs;
+    int pixelsTurnedOff = NUMBER_OF_PIXELS * ratio;
+
+    // Turn off
+    for (int i = 0; i < pixelsTurnedOff && i < NUMBER_OF_PIXELS; i++)
+    {
+      pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+    }
+
+    // Send the updated pixel colors to the hardware.
+    pixels.show();
+  }
+  else
+  {
+
+    for (int i = 0; i < NUMBER_OF_PIXELS; i++)
+    {
+      pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+    }
+    pixels.show(); // Send the updated pixel colors to the hardware.
+    set_state(TurnedOff);
+  }
+}
+
+void TurnOffAnimation2_Loop()
+{
+  unsigned long currentTime = millis();
+  unsigned long timeSinceStart = currentTime - stateStartTime;
+  if (timeSinceStart < turnOffAnimation.AnimationTimeMs)
+  {
+    // Do the turn off animation
+    float ratio = timeSinceStart / turnOffAnimation.AnimationTimeMs;
     int pixelsTurnedOff = NUMBER_OF_PIXELS * ratio / 2;
     int halfPoint = NUMBER_OF_PIXELS / 2;
 
-    // Turn on from the middle and out
+    // Turn off from the middle and out
 
     // Upper half
     for (int i = halfPoint; i < halfPoint + pixelsTurnedOff && i < NUMBER_OF_PIXELS; i++)
@@ -303,5 +359,224 @@ void TurnOffAnimation_Loop()
     }
     pixels.show(); // Send the updated pixel colors to the hardware.
     set_state(TurnedOff);
+  }
+}
+
+
+// https://github.com/nicolausYes/easing-functions/blob/master/src/easing.cpp
+float easeInSine(float t)
+{
+  return sin(1.5707963 * t);
+}
+
+float easeOutSine(float t)
+{
+  return 1 + sin(1.5707963 * (--t));
+}
+
+float easeInOutSine(float t)
+{
+  return 0.5 * (1 + sin(3.1415926 * (t - 0.5)));
+}
+
+float easeInQuad(float t)
+{
+  return t * t;
+}
+
+float easeOutQuad(float t)
+{
+  return t * (2 - t);
+}
+
+float easeInOutQuad(float t)
+{
+  return t < 0.5 ? 2 * t * t : t * (4 - 2 * t) - 1;
+}
+
+float easeInCubic(float t)
+{
+  return t * t * t;
+}
+
+float easeOutCubic(float t)
+{
+  return 1 + (--t) * t * t;
+}
+
+float easeInOutCubic(float t)
+{
+  return t < 0.5 ? 4 * t * t * t : 1 + (--t) * (2 * (--t)) * (2 * t);
+}
+
+float easeInQuart(float t)
+{
+  t *= t;
+  return t * t;
+}
+
+float easeOutQuart(float t)
+{
+  t = (--t) * t;
+  return 1 - t * t;
+}
+
+float easeInOutQuart(float t)
+{
+  if (t < 0.5)
+  {
+    t *= t;
+    return 8 * t * t;
+  }
+  else
+  {
+    t = (--t) * t;
+    return 1 - 8 * t * t;
+  }
+}
+
+float easeInQuint(float t)
+{
+  float t2 = t * t;
+  return t * t2 * t2;
+}
+
+float easeOutQuint(float t)
+{
+  float t2 = (--t) * t;
+  return 1 + t * t2 * t2;
+}
+
+float easeInOutQuint(float t)
+{
+  float t2;
+  if (t < 0.5)
+  {
+    t2 = t * t;
+    return 16 * t * t2 * t2;
+  }
+  else
+  {
+    t2 = (--t) * t;
+    return 1 + 16 * t * t2 * t2;
+  }
+}
+
+float easeInExpo(float t)
+{
+  return (pow(2, 8 * t) - 1) / 255;
+}
+
+float easeOutExpo(float t)
+{
+  return 1 - pow(2, -8 * t);
+}
+
+float easeInOutExpo(float t)
+{
+  if (t < 0.5)
+  {
+    return (pow(2, 16 * t) - 1) / 510;
+  }
+  else
+  {
+    return 1 - 0.5 * pow(2, -16 * (t - 0.5));
+  }
+}
+
+float easeInCirc(float t)
+{
+  return 1 - sqrt(1 - t);
+}
+
+float easeOutCirc(float t)
+{
+  return sqrt(t);
+}
+
+float easeInOutCirc(float t)
+{
+  if (t < 0.5)
+  {
+    return (1 - sqrt(1 - 2 * t)) * 0.5;
+  }
+  else
+  {
+    return (1 + sqrt(2 * t - 1)) * 0.5;
+  }
+}
+
+float easeInBack(float t)
+{
+  return t * t * (2.70158 * t - 1.70158);
+}
+
+float easeOutBack(float t)
+{
+  return 1 + (--t) * t * (2.70158 * t + 1.70158);
+}
+
+float easeInOutBack(float t)
+{
+  if (t < 0.5)
+  {
+    return t * t * (7 * t - 2.5) * 2;
+  }
+  else
+  {
+    return 1 + (--t) * t * 2 * (7 * t + 2.5);
+  }
+}
+
+float easeInElastic(float t)
+{
+  float t2 = t * t;
+  return t2 * t2 * sin(t * PI * 4.5);
+}
+
+float easeOutElastic(float t)
+{
+  float t2 = (t - 1) * (t - 1);
+  return 1 - t2 * t2 * cos(t * PI * 4.5);
+}
+
+float easeInOutElastic(float t)
+{
+  float t2;
+  if (t < 0.45)
+  {
+    t2 = t * t;
+    return 8 * t2 * t2 * sin(t * PI * 9);
+  }
+  else if (t < 0.55)
+  {
+    return 0.5 + 0.75 * sin(t * PI * 4);
+  }
+  else
+  {
+    t2 = (t - 1) * (t - 1);
+    return 1 - 8 * t2 * t2 * sin(t * PI * 9);
+  }
+}
+
+float easeInBounce(float t)
+{
+  return pow(2, 6 * (t - 1)) * abs(sin(t * PI * 3.5));
+}
+
+float easeOutBounce(float t)
+{
+  return 1 - pow(2, -6 * t) * abs(cos(t * PI * 3.5));
+}
+
+float easeInOutBounce(float t)
+{
+  if (t < 0.5)
+  {
+    return 8 * pow(2, 8 * (t - 1)) * abs(sin(t * PI * 7));
+  }
+  else
+  {
+    return 1 - 8 * pow(2, -8 * t) * abs(sin(t * PI * 7));
   }
 }
