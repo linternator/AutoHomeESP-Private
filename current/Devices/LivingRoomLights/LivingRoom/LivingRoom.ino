4e43d48558f6322fc936ded00bdde299205bbb6f
Jens Larsen <mr.javawolf@gmail.com>
2020-10-10 15:21:02 +0200

    Added: Better rainbow animation

--- current/Devices/LivingRoomLights/LivingRoom/LivingRoom.ino
+++ current/Devices/LivingRoomLights/LivingRoom/LivingRoom.ino
@@ -3,7 +3,6 @@
 /// - http://fastled.io/docs/
 
 #include <FastLED.h>
-
 #include <AutoHome.h>
 
 FASTLED_USING_NAMESPACE
@@ -27,7 +26,11 @@ CRGB ledstrip2[NUM_LEDS];
 CRGB ledstrip3[NUM_LEDS];
 CRGB ledstrip4[NUM_LEDS];
 
-#define BRIGHTNESS 255
+// #define BRIGHTNESS 255
+volatile int BRIGHTNESS = 255;
+volatile int SATURATION = 255;
+volatile float RAINBOW_SCALE = 0.5;
+volatile float RAINBOW_SPEED = 1;
 #define FRAMES_PER_SECOND 20
 
 AutoHome autohome;
@@ -45,8 +48,69 @@ void mqtt_callback(char *topic, byte *payload, unsigned int length)
       packet = packet + (char)payload[i];
     }
     Serial.print(packet);
+
+    Serial.println("]");
+
+    if (autohome.getValue(packet, ':', 0).equals("Brightness"))
+    {
+      BRIGHTNESS = autohome.getValue(packet, ':', 1).toInt();
+      FastLED.setBrightness(BRIGHTNESS);
+      mqtt_send_stats();
+    }
+
+    if (autohome.getValue(packet, ':', 0).equals("SATURATION"))
+    {
+      SATURATION = autohome.getValue(packet, ':', 1).toInt();
+      mqtt_send_stats();
+    }
+
+    if (autohome.getValue(packet, ':', 0).equals("RAINBOW_SCALE"))
+    {
+      RAINBOW_SCALE = autohome.getValue(packet, ':', 1).toFloat();
+      mqtt_send_stats();
+    }
+
+    if (autohome.getValue(packet, ':', 0).equals("RAINBOW_SPEED"))
+    {
+      RAINBOW_SPEED = autohome.getValue(packet, ':', 1).toFloat();
+      mqtt_send_stats();
+    }
+
+    if (autohome.getValue(packet, ':', 0).equals("stat"))
+    {
+      mqtt_send_stats();
+    }
+
+    // if (autohome.getValue(packet, ':', 0).equals("time_counter"))
+    //  {
+    //    time_counter = autohome.getValue(packet, ',', 1).toInt();
+    //    mqtt_send_stats();
+    //  }
+
+    // if (autohome.getValue(packet, ':', 0).equals("SET"))
+    //  {
+    //   WW = autohome.getValue(packet, ',', 1).toInt();
+    //   CW = autohome.getValue(packet, ',', 2).toInt();
+    //   AM = autohome.getValue(packet, ',', 3).toInt();
+
+    //    mqtt_send_stats();
+    //    changeColourRequested = true;
+    //  }
   }
-  Serial.println("]");
+}
+
+void mqtt_send_stats()
+{
+  String packet = "living room lights: "
+                  "brightness = " +
+                  String(BRIGHTNESS) + ", "
+                                       "SATURATION = " +
+                  String(SATURATION) + ", "
+                                       "RAINBOW_SCALE = " +
+                  String(RAINBOW_SCALE) + ", "
+                                          "switch stat = " +
+                  (digitalRead(ON_OFF_SWITCH_PIN)) + "";
+  autohome.sendPacket(packet.c_str());
 }
 
 void setup()
@@ -74,15 +138,15 @@ void setup()
   autohome.begin();
 }
 
-uint8_t time_counter = 0; // rotating "base color" used by many of the patterns
 unsigned long last_update_time = 0;
+unsigned long time_counter = 0; // rotating "base color" used by many of the patterns
 
 void loop()
 {
   autohome.loop();
   unsigned long current_time = millis();
 
-  if(abs(last_update_time - current_time) > 1000 / FRAMES_PER_SECOND)
+  if (abs(last_update_time - current_time) > 1000 / FRAMES_PER_SECOND)
   {
     last_update_time = current_time;
     time_counter += 1;
@@ -90,10 +154,10 @@ void loop()
     {
       rainbow(time_counter);
       //CRGB color = CRGB(0x00,0x00,0xff);
-      //SetColor(ledstrip1, color);
-      //SetColor(ledstrip2, color);
-      //SetColor(ledstrip3, color);
-      //SetColor(ledstrip4, color);
+      //SetColor(ledstrip1, CRGB(0xFF,0x00,0x00));
+      //SetColor(ledstrip2, CRGB(0x00,0xFF,0x00));
+      //SetColor(ledstrip3, CRGB(0x00,0x00,0xFF));
+      //SetColor(ledstrip4, CRGB(0xFF,0xFF,0xFF));
     }
     else
     {
@@ -101,6 +165,11 @@ void loop()
       SetColor(ledstrip2, CRGB::Black);
       SetColor(ledstrip3, CRGB::Black);
       SetColor(ledstrip4, CRGB::Black);
+      addGlitter(ledstrip1, 75);
+      addGlitter(ledstrip2, 75);
+      addGlitter(ledstrip3, 75);
+      addGlitter(ledstrip4, 75);
+
     }
 
     // send the 'leds' array out to the actual LED strip
@@ -108,13 +177,12 @@ void loop()
   }
 }
 
-void rainbow(uint8_t value)
+void rainbow(unsigned long time_counter)
 {
-  // FastLED's built-in rainbow generator
-  fill_rainbow(ledstrip1, NUM_LEDS, value, 7);
-  fill_rainbow(ledstrip2, NUM_LEDS, value + 64, 7);
-  fill_rainbow(ledstrip3, NUM_LEDS, value + 128, 7);
-  fill_rainbow(ledstrip4, NUM_LEDS, value + 192, 7);
+  fill_my_rainbow(ledstrip1, time_counter, 0);
+  fill_my_rainbow(ledstrip2, time_counter, 64);
+  fill_my_rainbow(ledstrip3, time_counter, 128);
+  fill_my_rainbow(ledstrip4, time_counter, 192);
 }
 
 void SetColor(struct CRGB *leds, const struct CRGB &color)
@@ -122,5 +190,24 @@ void SetColor(struct CRGB *leds, const struct CRGB &color)
   for (int i = 0; i < NUM_LEDS; i++)
   {
     leds[i] = color;
+  }
+}
+
+void fill_my_rainbow(struct CRGB *leds, unsigned long time_counter, int offset)
+{
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    /* The higher the value 4 the less fade there is and vice versa */
+    uint8_t hue = (uint8_t)(i * RAINBOW_SCALE + time_counter * RAINBOW_SPEED);
+    leds[i] = CHSV(hue + offset, SATURATION, BRIGHTNESS);
+  }
+}
+
+//glitter effect
+void addGlitter(struct CRGB *leds, fract8 chanceOfGlitter)
+{
+  if (random8() < chanceOfGlitter)
+  {
+    leds[random16(NUM_LEDS)] += CRGB::White;
   }
 }
