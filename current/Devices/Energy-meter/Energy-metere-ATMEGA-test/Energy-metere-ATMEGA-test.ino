023d78a4ba5c04600ac0eb7a72a112b3cc3e2afe
Jens Larsen <mr.javawolf@gmail.com>
2020-10-25 18:29:54 +0100

    Improved: there are now 2 distinct SoftwareSerial's

--- current/Devices/Energy-meter/Energy-metere-ATMEGA-test/Energy-metere-ATMEGA-test.ino
+++ current/Devices/Energy-meter/Energy-metere-ATMEGA-test/Energy-metere-ATMEGA-test.ino
@@ -10,39 +10,28 @@
 #define D8 (15)
 #define TX (1)
 
-#define BAUD_RATE 19200  // 57600
+#define BAUD_RATE 19200 // 57600
 
- SoftwareSerial swSer;
+SoftwareSerial VSerial;
+SoftwareSerial ASerial;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   // Important: the buffer size optimizations here, in particular the isrBufSize (11) that is only sufficiently
   // large to hold a single 8N1 word, are on the basis that any char written to the loopback SoftwareSerial
   // adapter gets read before another write is performed.
   // Block writes with a size greater than 1 would usually fail. Do not copy this into your own project without
   // reading the documentation.
-//   swSer.begin(BAUD_RATE, SWSERIAL_8N1, 4, 5, false, 95, 11);    // A // Voltage
-  swSer.begin(BAUD_RATE, SWSERIAL_8N1, 5, 4, false, 95, 11);   // B // Current
-
+  VSerial.begin(BAUD_RATE, SWSERIAL_8N1, 4, 5, false, 95, 11); // A // Voltage
+  ASerial.begin(BAUD_RATE, SWSERIAL_8N1, 5, 4, false, 95, 11); // B // Current
 
   Serial.println(PSTR("\nSoftware serial test started"));
-
-  for (char ch = ' '; ch <= 'z'; ch++) {
-    swSer.write(ch);
-  }
-  swSer.println();
 }
 
-void loop() {
-  while (swSer.available() > 0) {
-    Serial.write(swSer.read());
-    yield();
-  }
-  while (Serial.available() > 0) {
-    int Byte = 0x00;
-    Byte = Serial.read();
-    swSer.write(Byte,sizeof(Byte));   // MUST SEND A 1 HEX BYTE ONLY.
-    yield();
-  }
-
+void loop()
+{
+  uint8_t coursSpeed = random(0, 255);
+  ASerial.write(coursSpeed);
+  delay(2000);
 }
